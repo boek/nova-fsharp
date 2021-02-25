413c66d56d89f5a7b521ee5173c8ece1697f3d06
Jeff Boek <jeff@jeffboek.com>
2021-02-24 16:21:21 -0800

    Adds scripts, fixes some syntax

---
+++ FSharp.novaextension/Scripts/main.js
@@ -0,0 +1,80 @@
+
+var langserver = null;
+
+exports.activate = function() {
+    // Do work when the extension is activated
+    langserver = new ExampleLanguageServer();
+}
+
+exports.deactivate = function() {
+    // Clean up state before the extension is deactivated
+    if (langserver) {
+        langserver.deactivate();
+        langserver = null;
+    }
+}
+
+
+class ExampleLanguageServer {
+    constructor() {
+        // Observe the configuration setting for the server's location, and restart the server on change
+        nova.config.observe('example.language-server-path', function(path) {
+            this.start(path);
+        }, this);
+    }
+    
+    deactivate() {
+        this.stop();
+    }
+    
+    start(path) {
+        if (this.languageClient) {
+            this.languageClient.stop();
+            nova.subscriptions.remove(this.languageClient);
+        }
+        
+        // Use the default server path
+        if (!path) {
+            path = nova.extension.path + "/bin/launch.sh";
+        }
+        
+        // Create the client
+        var serverOptions = {
+            path: path
+        };
+        var clientOptions = {
+            // The set of document syntaxes for which the server is valid
+            syntaxes: ['F#', 'fsharp', 'fs'],
+            initializationOptions: {
+                "AutomaticWorkspaceInit": true
+            }
+        };
+        var client = new LanguageClient('example-langserver', 'Example Language Server', serverOptions, clientOptions);
+        
+        try {
+            // Start the client
+            console.log("lol2")
+            client.start();
+            // Add the client to the subscriptions to be cleaned up
+            nova.subscriptions.add(client);
+            this.languageClient = client;
+            client.sendNotification("initialize", {})
+        }
+        catch (err) {
+            // If the .start() method throws, it's likely because the path to the language server is invalid
+            
+            if (nova.inDevMode()) {
+                console.error(err);
+            }
+        }
+    }
+    
+    stop() {
+        if (this.languageClient) {
+            this.languageClient.stop();
+            nova.subscriptions.remove(this.languageClient);
+            this.languageClient = null;
+        }
+    }
+}
+
